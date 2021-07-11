2c8e52d6428a496056dd6f0415e498060fb76946
Samira Milne <milne.samira@GMAIL.COM>
2021-07-10 19:15:52 -0500

    added layout wrapper and data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES=[
+  {id: 'q1', author: 'Bob', text: 'Learning is fun!'},
+  {id: 'q2', author: 'Bill', text: 'React is awesome!'},
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES}/>;
 };
 
 export default AllQuotes;
