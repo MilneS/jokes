70b4858b473bfc06b5e72f643027ffc571953542
Samira Milne <milne.samira@GMAIL.COM>
2021-07-10 19:31:49 -0500

    output data in Details + not found page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,13 +2,24 @@ import { Fragment } from "react";
 import { useParams } from "react-router";
 import { Route } from "react-router";
 import Comments from '../components/comments/Comments'
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
+
+const DUMMY_QUOTES=[
+  {id: 'q1', author: 'Bob', text: 'Learning is fun!'},
+  {id: 'q2', author: 'Bill', text: 'React is awesome!'},
+]
 
 const QuoteDetail = () => {
   const params = useParams();
+const quote=DUMMY_QUOTES.find(quote=>quote.id === params.quoteId)
+
+if(!quote){
+  return <p>No quote found.</p>
+}
+
   return(
   <Fragment>
-    <h1>Quote Detail Page</h1>
-    <p>{params.quoteId}</p>
+    <HighlightedQuote text={quote.text} author={quote.author}/>
     <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
     </Route>
